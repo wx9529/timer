dc8eb5d3848c119b6595fea0afff2238f452240c
wx9529 <xinwang213z@gmail.com>
2022-03-15 16:01:03 -0700

    Simple_timer

--- timer1.js
+++ timer1.js
@@ -1,9 +1,3 @@
-// const arr = process.argv
-//   .slice(2)
-//   .sort((a, b) => a - b)
-//   .map((value) => {
-//     return value * 1000;
-//   });
 let input = [];
 for (let item of process.argv.slice(2)) {
   if (Number(item) && item >= 0) {
@@ -14,5 +8,4 @@ for (let i = 0; i < input.length; i++) {
   setTimeout(() => {
     process.stdout.write("\x07");
   }, input[i] * 1000);
-  console.log(input[i] * 1000);
 }
